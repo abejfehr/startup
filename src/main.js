15db8402368d06d15fd2f0fc17166f3599998f79
Abe Fehr <abe@studocu.com>
2016-07-08 10:19:16 +0200

    Make dist and src folders match; stop ignoring .git

--- src/main.js
+++ src/main.js
@@ -8,15 +8,43 @@
 class StartupGame {
     constructor () {
         // The amount of points the player currently has
-        this.points = 0;
+        this.saveData = {
+            points: 0
+        }
 
         // Add the onClick handler to the whole body
         document.body.addEventListener('click', this.addPoints.bind(this, 1));
+
+        this.load().then((loadedData) => {
+            this.saveData = loadedData;
+            document.getElementById('points').innerText = this.saveData.points;
+        });
     }
 
     addPoints (howMany) {
-        this.points += howMany;
-        document.getElementById('points').innerText = this.points;
+        this.saveData.points += howMany;
+        document.getElementById('points').innerText = this.saveData.points;
+        this.save();
+    }
+
+    /**
+     * Saves the game's data asynchronously to LocalStorage
+     */
+    save () {
+        setTimeout(() => {
+            localStorage.setItem("saveData", btoa(JSON.stringify(this.saveData)));
+        }, 0);
+    }
+
+    /**
+     * Loads the game's data asynchronously from LocalStorage
+     */
+    load () {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(JSON.parse(atob(localStorage.getItem("saveData"))));
+            }, 0);
+        });
     }
 }
 
