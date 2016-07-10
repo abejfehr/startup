105dd984dda298ff7582ced6f1f88781e910abe2
Abe Fehr <abe@studocu.com>
2016-07-10 18:48:41 +0200

    Add super basic version of workers

--- src/main.js
+++ src/main.js
@@ -1,20 +1,29 @@
+ /* jshint esversion: 6 */
+
+ /**
+ *
+ */
+class Worker {
+    constructor (pointsPerMinute) {
+        this.pointsPerMinute = pointsPerMinute;
+    }
+}
+
 /**
  * Purpose: The main game loop
  * Source:  main.js
  */
 
-/* jshint esversion: 6 */
-
 class StartupGame {
     constructor () {
-
         /**
          * The various data about the game that can be immediately saved/loaded
          */
         this.saveData = {
             points: 0,
-            queuedPoints: 0
-        }
+            queuedPoints: 0,
+            workers: []
+        };
 
         /**
          * The timestamp since the last animation frame step. This is only used
@@ -27,7 +36,7 @@ class StartupGame {
          * arbitrary, it should really be calculated dynamically each step from
          * powerups or whatever the user has
          */
-        this.pointsPerMinute = 6;
+        this.pointsPerMinute = 0;
 
         /**
          * The number of points to add per click. 1 is arbitrary, but it's a
@@ -40,12 +49,11 @@ class StartupGame {
             this.saveData.queuedPoints += this.pointsPerClick;
         });
 
-        // Loads the data and kicks off the timer as soon as the data is loaded.
-        this.load().then((loadedData) => {
-            this.saveData = loadedData;
-            window.requestAnimationFrame(this.step.bind(this));
+        // Add the click event listener to the worker 1 button
+        document.getElementById('worker1').addEventListener('click', (e) => {
+            e.stopPropagation();
+            this.saveData.workers.push(new Worker(30));
         });
-
     }
 
     /**
@@ -53,6 +61,7 @@ class StartupGame {
      */
     save () {
         setTimeout(() => {
+            // btoa() is used for basic encryption
             localStorage.setItem("saveData", btoa(JSON.stringify(this.saveData)));
         }, 0);
     }
@@ -61,11 +70,32 @@ class StartupGame {
      * Loads the game's data asynchronously from LocalStorage.
      */
     load () {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             setTimeout(() => {
-                resolve(JSON.parse(atob(localStorage.getItem("saveData"))));
+                if (localStorage.getItem("saveData") === null) {
+                  reject();
+                } else {
+                  resolve(JSON.parse(atob(localStorage.getItem("saveData"))));
+                }
             }, 0);
         });
+    }
+
+    start () {
+      // Loads the data and kicks off the timer as soon as the data is loaded.
+      this.load()
+      .then((loadedData) => {
+          this.saveData = loadedData;
+          /**
+           * TODO: Workers aren't persistent yet, we need some way of storing
+           * them that aren't instances of the objects themselves.
+           */
+          this.saveData.workers = [];
+          window.requestAnimationFrame(this.step.bind(this));
+      })
+      .catch(() => {
+          window.requestAnimationFrame(this.step.bind(this));
+      });
     }
 
     /**
@@ -73,6 +103,16 @@ class StartupGame {
      */
     updatePoints () {
         document.getElementById('points').innerText = this.saveData.points;
+    }
+
+    getPointsPerMinute () {
+        var n = 0;
+        if (this.saveData.workers.map) {
+            this.saveData.workers.map((worker) => {
+                n += worker.pointsPerMinute;
+            });
+        }
+        return n;
     }
 
     /**
@@ -84,10 +124,10 @@ class StartupGame {
         this.lastTimestamp = timestamp;
 
         // Figure out how many points to add
-        this.saveData.queuedPoints += this.pointsPerMinute * progress;
+        this.saveData.queuedPoints += this.getPointsPerMinute() * progress;
 
         // Add the floor of the number of points to add
-        this.saveData.points += ~~this.saveData.queuedPoints;
+        this.saveData.points += Math.floor(this.saveData.queuedPoints);
         this.saveData.queuedPoints %= 1;
 
         // Saves the game, probably way too often
@@ -105,3 +145,4 @@ class StartupGame {
  * Let's get this party started!
  */
 var game = new StartupGame();
+game.start();
