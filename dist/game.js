803aaa83b3add67fd3cf80423eab7a1f7e08433a
Scott Andrechek <scottandrechek@gmail.com>
2016-09-09 16:33:21 -0400

    Header change

--- dist/game.js
+++ dist/game.js
@@ -143,9 +143,13 @@ game.start();
  * Purpose: All worker classes
  * Source:  worker.js
  */
- 
+
  /* jshint esversion: 6 */
 
+/*
+ * Should never be initialized.
+ * Used as a base class for other worker types.
+ */
 class Worker {
    constructor (pointsPerMinute) {
        this.pointsPerMinute = pointsPerMinute;
--- src/worker.js
+++ src/worker.js
@@ -2,9 +2,13 @@
  * Purpose: All worker classes
  * Source:  worker.js
  */
- 
+
  /* jshint esversion: 6 */
 
+/*
+ * Should never be initialized.
+ * Used as a base class for other worker types.
+ */
 class Worker {
    constructor (pointsPerMinute) {
        this.pointsPerMinute = pointsPerMinute;
