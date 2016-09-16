e4162bd14fadeef69d1822ad3ea96e97428e3b7a
Scott Andrechek <scottandrechek@gmail.com>
2016-09-16 14:55:35 -0400

    Changed tabs.

--- src/main.js
+++ src/main.js
@@ -6,125 +6,127 @@
  /* jshint esversion: 6 */
 
 class StartupGame {
-    constructor () {
-        /**
-         * The various data about the game that can be immediately saved/loaded
-         */
-        this.saveData = {
-            points: 1,
-            queuedPoints: 0,
-            workers: []
-        };
+  constructor () {
+    /**
+     * The various data about the game that can be immediately saved/loaded
+     */
+    this.gameData = {
+        views: 1,
+        queuedViews: 0,
+        workers: [],
+        skills: []
+    };
 
-        /**
-         * The timestamp since the last animation frame step. This is only used
-         * by the step function.
-         */
-        this.lastTimestamp = 0;
+    this.views = 1;
 
-        /**
-         * The number of points to add every minute of the game. 6 is totally
-         * arbitrary, it should really be calculated dynamically each step from
-         * powerups or whatever the user has
-         */
-        this.pointsPerMinute = 0;
+    this.queuedViews = 1;
 
-        /**
-         * The number of points to add per click. 1 is arbitrary, but it's a
-         * good starting value.
-         */
-        this.pointsPerClick = 1;
+    this.workers = [];
 
-        // Add the click handler to the whole body
-        document.body.addEventListener('click', () => {
-            this.saveData.queuedPoints += this.pointsPerClick;
-        });
-    }
+    this.skills = [];
 
     /**
-     * Saves the game's data asynchronously to LocalStorage
+     * The timestamp since the last animation frame step. This is only used
+     * by the step function.
      */
-    save () {
+    this.lastTimestamp = 0;
+
+    /**
+     * The number of views to add every minute of the game. 6 is totally
+     * arbitrary, it should really be calculated dynamically each step from
+     * powerups or whatever the user has
+     */
+    this.viewsPerMinute = 0;
+
+    /**
+     * The number of views to add per click. 1 is arbitrary, but it's a
+     * good starting value.
+     */
+    this.viewsPerClick = 1;
+
+    // Add the click handler to the whole body
+    document.body.addEventListener('click', () => {
+        this.gameData.queuedViews += this.viewsPerClick;
+    });
+  }
+
+  /**
+   * Saves the game's data asynchronously to LocalStorage
+   */
+  save () {
+    setTimeout(() => {
+        // btoa() is used for basic encryption
+        localStorage.setItem("saveData", btoa(JSON.stringify(this.gameData)));
+    }, 0);
+  }
+
+  /**
+   * Loads the game's data asynchronously from LocalStorage.
+   */
+  load () {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            // btoa() is used for basic encryption
-            localStorage.setItem("saveData", btoa(JSON.stringify(this.saveData)));
+            if (localStorage.getItem("saveData") === null) {
+              reject();
+            } else {
+              resolve(JSON.parse(atob(localStorage.getItem("saveData"))));
+            }
         }, 0);
+    });
+  }
+
+  start () {
+    // Loads the data and kicks off the timer as soon as the data is loaded.
+    this.load()
+    .then((loadedData) => {
+      this.gameData = loadedData;
+      this.gameData.views += 1;
+      /**
+       * TODO: Workers aren't persistent yet, we need some way of storing
+       * them that aren't instances of the objects themselves.
+       */
+      this.gameData.workers = [];
+      loadedData.workers.push(new Worker("First worker", "Test worker", 1, function(level) { return level*60; }));
+      window.requestAnimationFrame(this.step.bind(this));
+    })
+    .catch(() => {
+      window.requestAnimationFrame(this.step.bind(this));
+    });
+  }
+
+  /**
+   * Updates the views on the screen.
+   */
+  updateViews () {
+    document.getElementById('views').innerText = this.gameData.views;
+  }
+
+  /**
+   * The game loop that handles adding views. Takes a DOMHighResTimeStamp.
+   */
+  step (timestamp) {
+    // Get the time difference in milleseconds since the last timestamp
+    var progress = (timestamp - this.lastTimestamp) / 1000 / 60;
+    this.lastTimestamp = timestamp;
+
+    // Add more views to queue
+    for (let i = 0; i < this.gameData.workers.length; i++) {
+      this.gameData.queuedViews = this.gameData.workers[i].getRate() * progress;
     }
 
-    /**
-     * Loads the game's data asynchronously from LocalStorage.
-     */
-    load () {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (localStorage.getItem("saveData") === null) {
-                  reject();
-                } else {
-                  resolve(JSON.parse(atob(localStorage.getItem("saveData"))));
-                }
-            }, 0);
-        });
-    }
+    // Add the floor of the number of views to add
+    this.gameData.views += Math.floor(this.gameData.queuedViews);
+    this.gameData.queuedViews %= 1;
 
-    start () {
-      // Loads the data and kicks off the timer as soon as the data is loaded.
-      this.load()
-      .then((loadedData) => {
-          this.saveData = loadedData;
-          this.saveData.points += 1;
-          /**
-           * TODO: Workers aren't persistent yet, we need some way of storing
-           * them that aren't instances of the objects themselves.
-           */
-          this.saveData.workers = [];
-          window.requestAnimationFrame(this.step.bind(this));
-      })
-      .catch(() => {
-          window.requestAnimationFrame(this.step.bind(this));
-      });
-    }
+    // Saves the game, probably way too often
+    this.save();
 
-    /**
-     * Updates the points on the screen.
-     */
-    updatePoints () {
-        document.getElementById('points').innerText = this.saveData.points;
-    }
+    // Writes the number of views to the screen
+    this.updateViews();
 
-    getPointsPerMinute () {
-        var n = 0;
-        if (this.saveData.workers.map) {
-            this.saveData.workers.map((worker) => {
-                n += worker.pointsPerMinute;
-            });
-        }
-        return n;
-    }
-
-    /**
-     * The game loop that handles adding points. Takes a DOMHighResTimeStamp.
-     */
-    step (timestamp) {
-        // Get the time difference in milleseconds since the last timestamp
-        var progress = (timestamp - this.lastTimestamp) / 1000 / 60;
-        this.lastTimestamp = timestamp;
-
-        // Figure out how many points to add
-        this.saveData.queuedPoints += this.getPointsPerMinute() * progress;
-
-        // Add the floor of the number of points to add
-        this.saveData.points += Math.floor(this.saveData.queuedPoints);
-        this.saveData.queuedPoints %= 1;
-
-        // Saves the game, probably way too often
-        this.save();
-
-        // Writes the number of points to the screen
-        this.updatePoints();
-
-        // Recursively do this again
-        window.requestAnimationFrame(this.step.bind(this));
-    }
+    // Recursively do this again
+    window.requestAnimationFrame(this.step.bind(this));
+  }
 }
 
 /**
--- src/worker.js
+++ src/worker.js
@@ -5,12 +5,25 @@
 
  /* jshint esversion: 6 */
 
-/*
- * Should never be initialized.
- * Used as a base class for other worker types.
- */
 class Worker {
-   constructor (pointsPerMinute) {
-       this.pointsPerMinute = pointsPerMinute;
-   }
+  /**
+   * @param {String} n - The name
+   * @param {String} desc - The description
+   * @param {Integer} level - The level of the worker
+   * @param {Function} rate
+   *  Calculates the rate of views for the current level
+   *  or the given level.
+   *  @param {Integer} level - to be used to calculate rate of views at this level (optional)
+   *  @return {Number} _ - The rate of views expressed as a positive rational number
+   */
+  constructor (n, desc, level, rate) {
+    this.name = n;
+    this.desc = desc;
+    this.level = level;
+    this.rate = rate;
+  }
+
+  getRate (l) {
+    return (!l) ? this.rate(this.level) : this.rate(l);
+  }
 }
