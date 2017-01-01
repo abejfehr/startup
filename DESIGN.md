40f027cecfeb86b854637b65e79b5dd4ac06daeb
Abe Fehr <abe.fehr@gmail.com>
2017-01-01 16:40:10 -0500

    Add some more unit tests for basic saving loading

--- DESIGN.md
+++ DESIGN.md
@@ -1,7 +1,31 @@
 # [Startup](https://www.abefehr.com/startup/)
 
+## Basic Idea
+
 The game begins by showing only one line of text on the screen.
 
 > This page has been viewed 1 time.
 
 Out of curiousity, and to see if the view counter is correct, the user then refreshes the page and, to their surprise, the view count should go up by 1 each time. They'll do that for 5 views or so and then a table will appear showing them the workers they have.
+
+## Save Data
+
+The game's data is all saved in localStorage, in the browser. This means that sessions aren't transferrable from machine to machine, and there's nothing saved online.
+
+The sava data is a base64 encoded JSON object, with the following attributes:
+
+* views - The number of views that are currently being displayed on the screen
+* queuedViews - The fractional number of views that will be added to the screen on the next tick
+* workers - The list of workers that the user has. Each worker is also a JSON object and gets recreated dynamically on each load
+
+## Worker Data
+
+The worker data is the data for each individual worker.
+
+## The Ticks
+
+Ticks are times in which the game data is updated. Ticks occur on animation frames, so they don't happen in consistent time intervals.
+
+During each tick, the game gets the amount of views to add to the view counter, and queues them. The floor of the queuedViews are then added to the counter, while the remainder is kept for the next tick.
+
+The game is saved during each tick.
--- test/savemanager.spec.babel.js
+++ test/savemanager.spec.babel.js
@@ -1,28 +1,52 @@
-import { expect } from 'chai';
+// Assertions
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+const expect = chai.use(chaiAsPromised).expect;
+const should = chai.use(chaiAsPromised).should;
+
+// Mock Libraries
+import 'mock-local-storage'
+import {atob, btoa} from 'abab'
+
+// Class Under Test
 import SaveManager from '../target/savemanager';
 
 describe('the save manager', () => {
-  // Basics
-  it('should be instantiable', () => {
-		var saveManager = new SaveManager();
-		expect(saveManager).to.not.be.null;
-	});
+  describe('the basics', () => {
+    it('should be instantiable', () => {
+  		var saveManager = new SaveManager();
+  		expect(saveManager).to.not.be.null;
+  	});
+  });
 
-  // Loading
-  it('should have functionality for loading previously saved data', () => {
-		var saveManager = new SaveManager();
-		expect(saveManager.load).to.not.be.undefined;
-	});
+  describe('loading data', () => {
+    it('should have functionality for loading previously saved data', () => {
+  		var saveManager = new SaveManager();
+  		expect(saveManager.load).to.not.be.undefined;
+  	});
 
-  it('should load a game object when there is one in WebStorage', () => {
-		var saveManager = new SaveManager();
-		expect(saveManager.load).to.not.be.undefined;
-	});
+    it('should load a game object when there is one in localStorage', () => {
+      localStorage.setItem("saveData", btoa(JSON.stringify({views: 3, queuedViews: 0.5})));
 
-  // Saving
-  it('should have functionality for saving new data', () => {
-		var saveManager = new SaveManager();
-		expect(saveManager.save).to.not.be.undefined;
-	});
+  		var saveManager = new SaveManager();
+      expect(saveManager.load()).to.eventually.have.property('views');
+      expect(saveManager.load()).to.eventually.have.property('queuedViews');
+  	});
+
+    it('should reject a promise when there is nothing in localStorage', () => {
+      expect(1).to.be(2);
+    });
+  });
+
+  describe('saving data', () => {
+    it('should have functionality for saving new data', () => {
+  		var saveManager = new SaveManager();
+  		expect(saveManager.save).to.not.be.undefined;
+  	});
+
+    it('should save a given saveObject into localStorage', () => {
+      expect(1).to.be(2);
+    });
+  });
 
 });
