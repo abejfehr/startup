fe305cafc9da25e010d79ef3594cc75a2f405141
Abe Fehr <abe.fehr@gmail.com>
2017-01-04 02:48:52 -0500

    Test with an ad

--- src/view/Master.js
+++ src/view/Master.js
@@ -8,6 +8,16 @@ class Master extends React.Component {
     super(props);
   }
 
+  componentDidMount () {
+    // Add the Google AdSense script
+    var google = document.createElement('script');
+    google.setAttribute('src', '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+    document.head.appendChild(google);
+
+    // Run the script that Google has in their snippet
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }
+
   render () {
     var classNames = this.props.workers.length >= 1 ? 'css1' : '';
 
@@ -34,6 +44,10 @@ class Master extends React.Component {
               <div className="footer">
                 <hr />
                 <p>Copyright &copy; {(new Date()).getFullYear()} Your Startup. All Rights Reserved</p>
+                <ins className="adsbygoogle"
+                     style={{ display: 'inline-block', width: '728px', height: '90px' }}
+                     data-ad-client="ca-pub-4885767461778395"
+                     data-ad-slot="4261754677"></ins>
               </div> :
               <div />
             }
