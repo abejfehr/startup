8ca221aaf6ddd231cb8cf0b22b72540c0ef0f8dd
Abe Fehr <abe.fehr@gmail.com>
2017-01-06 18:49:29 -0500

    Disable Analytics on localhost; small CSS changes

--- src/main.js
+++ src/main.js
@@ -24,13 +24,15 @@ if ('serviceWorker' in navigator) {
 /**
  * Add some Analytics
  */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+if (location.href.indexOf('localhost') < 0) {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-30977471-1', 'auto');
-ga('send', 'pageview');
+  ga('create', 'UA-30977471-1', 'auto');
+  ga('send', 'pageview');
+}
 
 /**
  * Let's get this party started!
