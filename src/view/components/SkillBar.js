d7dc5dbd569f6244bd7e6ecd7e850889d16831b3
Abe Fehr <abe.fehr@gmail.com>
2017-01-05 23:55:18 -0500

    Fix dumbass error

--- src/view/components/SkillBar.js
+++ src/view/components/SkillBar.js
@@ -9,26 +9,26 @@ class SkillBar extends React.Component {
     super(props);
   }
 
-  // TODO: Write this using a reduce function
-  hasAllPrereqs (prereqs) {
-    var passes = true;
-    for (let prereq of prereqs) {
-      passes = passes && this.props.skills.indexOf(prereq);
-    }
-    return passes;
-  }
-
   render () {
     if (this.props.totalViews < 5) {
       return <div />;
     }
 
+    var props = this.props;
+    var hasAllPrereqs = (prereqs) => {
+      var passes = true;
+      for (let prereq of prereqs) {
+        passes = passes && (this.props.skills.indexOf(prereq) > -1);
+      }
+      return passes;
+    }
+
     var list = skills.reduce((acc, cur, i) => {
       if (
-        ! this.props.skills.find(el => el == cur.id) && // Only show the skill if we have it
-        this.props.totalViews >= cur.reveal && // and if we don't have the views to show it yet
-        (!cur.team || (cur.team && this.props.teams[cur.team] && this.props.teams[cur.team].workers.length > 0)) && // Or if it's for a team and we don't have any members of that team yet
-        (!cur.prerequisites || this.hasAllPrereqs(cur.prerequisites)) // And make sure the prerequisites are met
+        ! props.skills.find(el => el == cur.id) && // Only show the skill if we have it
+        props.totalViews >= cur.reveal && // and if we don't have the views to show it yet
+        (!cur.team || (cur.team && props.teams[cur.team] && props.teams[cur.team].workers.length > 0)) && // Or if it's for a team and we don't have any members of that team yet
+        (!cur.prerequisites || hasAllPrereqs(cur.prerequisites)) // And make sure the prerequisites are met
       ) {
         var item = <SkillItem skill={cur} key={i} onSkillPurchased={this.props.onSkillPurchased} />
         return acc.concat(item);
