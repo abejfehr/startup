b2f2c59cb6b4e45ada85efcaf17e64f28269b876
Abe Fehr <abe.fehr@gmail.com>
2017-01-07 15:59:38 -0500

    Separate shit into pages

--- src/view/Master.js
+++ src/view/Master.js
@@ -4,38 +4,29 @@ import SkillBar from './components/SkillBar';
 import WorkersTable from './components/WorkersTable';
 import Modal from './components/Modal';
 
+import Basic from './pages/Basic';
+import Search from './pages/Search';
+import Social from './pages/Social';
+import Startup from './pages/Startup';
+
 class Master extends Component {
 
   constructor (props) {
     super(props);
-
-    this.state = {
-      showModal: false,
-      shown: !this.props.skills.find(el => el == 'video' || el == 'social'),
-    };
-  }
-
-  componentWillUpdate () {
-    if (!this.state.shown && this.props.totalViews > 1000) {
-      this.setState({ showModal: true, shown: true });
-    }
   }
 
   render () {
+
+    var Page = Basic;
+    if (this.props.skills.indexOf('social') > -1) {
+      Page = Social;
+    } else if (this.props.skills.indexOf('search') > -1) {
+      Page = Search;
+    } else if (this.props.skills.indexOf('startup') > -1) {
+      Page = Startup;
+    }
+
     return <div className={"website " + this.props.skills.join(' ')}>
-            <Modal visible={this.state.showModal}>
-              <p>Inspirational Message</p>
-              <div className="site-options">
-                <div className="site-option">
-                  <h2>Video</h2>
-                  <a href="javascript:void(0)" onClick={() => this.setState({ shown: true, showModal: false }, () => this.props.onChoice('video'))}>Do this</a>
-                </div>
-                <div className="site-option">
-                  <h2>Social Media</h2>
-                  <a href="javascript:void(0)" onClick={() => this.setState({ shown: true, showModal: false }, () => this.props.onChoice('social'))}>Do this</a>
-                </div>
-              </div>
-            </Modal>
             <SkillBar
               totalViews={this.props.totalViews}
               views={this.props.views}
@@ -43,60 +34,15 @@ class Master extends Component {
               skills={this.props.skills}
               onSkillPurchased={this.props.onSkillPurchased} />
             <div className="container">
-              { this.props.skills.find(el => el == 'html2') ?
-                <div className="header">
-                  <h1 className="headline">{ this.props.skills.find(el => el == "startup") ? 'Startup' : 'My Website' }</h1>
-                  <p className="tagline">{ this.props.skills.find(el => el == "startup") ? 'Serious business' : 'My personal page' }</p>
-                  <hr />
-                </div> :
-                <div />
-              }
-              { this.props.totalViews > 0 ?
-                <div>
-                  <p>
-                    This page has been viewed {this.props.views} time{this.props.views !== 1 ? 's' : ''}.
-                  </p>
-                  { this.props.viewsPerSecond > 0 ?
-                    <p>
-                      You are earning {this.props.viewsPerSecond.toFixed(1)} view{this.props.viewsPerSecond !== 1 ? 's' : ''} per second.
-                    </p> :
-                    <div />
-                  }
-                </div> :
-                <div />
-              }
-              { this.props.skills.find(el => el == 'html2') && ! this.props.skills.find(el => el == 'startup') ?
-                <img src="assets/pikachu.gif" /> :
-                <div />
-              }
-              { this.props.skills.find(el => el == 'html2') ?
-                <div className="footer">
-                  <hr />
-                  <p>
-                    Copyright &copy; {(new Date()).getFullYear()} { this.props.skills.find(el => el == "startup") ? 'Startup' : 'My Website' }. All Rights Reserved. <a href="javascript:void(0)" onClick={() => this.props.onReset()}>
-                      Reset
-                    </a>
-                  </p>
-                  { this.props.skills.find(el => el == 'monetize') ?
-                    <div style={{
-                      display: 'inline-block',
-                      width: '728px',
-                      height: '90px',
-                      border: '1px solid black',
-                      backgroundColor: '#aaa'
-                    }} /> :
-                    <div />}
-                </div> :
-                <div />
-              }
-              </div>
-              { this.props.skills.find(el => el == 'startup') ?
-                <WorkersTable
-                  workers={this.props.workers}
-                  teams={this.props.teams}
-                  onHire={this.props.onHire} /> :
-                <div />
-              }
+              <Page {...this.props} />
+            </div>
+            { this.props.skills.find(el => el == 'startup') ?
+              <WorkersTable
+                workers={this.props.workers}
+                teams={this.props.teams}
+                onHire={this.props.onHire} /> :
+              <div />
+            }
             </div>
   }
 
---
+++ src/view/pages/Basic.js
@@ -0,0 +1,62 @@
+import { h, Component } from 'preact';
+
+class Basic extends Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return  <div>
+            { this.props.skills.find(el => el == 'html2') ?
+              <div className="header">
+                <h1 className="headline">{ this.props.skills.find(el => el == "startup") ? 'Startup' : 'My Website' }</h1>
+                <p className="tagline">{ this.props.skills.find(el => el == "startup") ? 'A serious business.' : 'My personal page' }</p>
+                <hr />
+              </div> :
+              <div />
+            }
+            { this.props.totalViews > 0 ?
+              <div>
+                <p>
+                  This page has been viewed {this.props.views} time{this.props.views !== 1 ? 's' : ''}.
+                </p>
+                { this.props.viewsPerSecond > 0 ?
+                  <p>
+                    You are earning {this.props.viewsPerSecond.toFixed(1)} view{this.props.viewsPerSecond !== 1 ? 's' : ''} per second.
+                  </p> :
+                  <div />
+                }
+              </div> :
+              <div />
+            }
+            { this.props.skills.find(el => el == 'html2') && ! this.props.skills.find(el => el == 'startup') ?
+              <img src="assets/pikachu.gif" /> :
+              <div />
+            }
+            { this.props.skills.find(el => el == 'html2') ?
+              <div className="footer">
+                <hr />
+                <p>
+                  Copyright &copy; {(new Date()).getFullYear()} { this.props.skills.find(el => el == "startup") ? 'Startup' : 'My Website' }. All Rights Reserved. <a href="javascript:void(0)" onClick={() => this.props.onReset()}>
+                    Reset
+                  </a>
+                </p>
+                { this.props.skills.find(el => el == 'monetize') ?
+                  <div style={{
+                    display: 'inline-block',
+                    width: '728px',
+                    height: '90px',
+                    border: '1px solid black',
+                    backgroundColor: '#aaa'
+                  }} /> :
+                  <div />}
+              </div> :
+              <div />
+            }
+            </div>
+  }
+
+}
+
+export default Basic;
