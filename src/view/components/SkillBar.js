456bcdec2683cf9c8905749686cd2f64ed360af2
Abe Fehr <abe.fehr@gmail.com>
2017-01-08 18:54:58 -0500

    Make early skills free and look more basic

--- src/view/components/SkillBar.js
+++ src/view/components/SkillBar.js
@@ -30,7 +30,7 @@ class SkillBar extends Component {
         (!cur.team || (cur.team && props.teams[cur.team] && props.teams[cur.team].workers.length > 0)) && // Or if it's for a team and we don't have any members of that team yet
         (!cur.prerequisites || hasAllPrereqs(cur.prerequisites)) // And make sure the prerequisites are met
       ) {
-        var item = <SkillItem clickable={props.skills.find(el => el == 'css1')} skill={cur} key={i} onSkillPurchased={this.props.onSkillPurchased} />
+        var item = <SkillItem basic={!this.props.skills.find(el => el == 'html2')} clickable={props.skills.find(el => el == 'css1')} skill={cur} key={i} onSkillPurchased={this.props.onSkillPurchased} />
         return acc.concat(item);
       } else {
         return acc;
@@ -38,13 +38,20 @@ class SkillBar extends Component {
     }, []);
 
     return <div className="skill-bar">
-      <h2 className="skill-bar-title">Skills</h2>
+      { this.props.skills.find(el => el == 'html2') ?
+        <h2 className="skill-bar-title">Skills</h2> :
+        <div />
+      }
       <div>
         { list.length ?
           list :
           <div className="no-skills">There are currently no skills available.</div>
         }
       </div>
+      { this.props.skills.find(el => el == 'html2') ?
+        <div /> :
+        <br />
+      }
     </div>;
   }
 
--- src/view/components/SkillItem.js
+++ src/view/components/SkillItem.js
@@ -8,11 +8,21 @@ class SkillItem extends Component {
 
   render () {
     return <div className="skill-bar-item" onClick={() => this.props.clickable && this.props.onSkillPurchased(this.props.skill)}>
-      <div className="skill-title">{this.props.skill.name}</div>
+      <div className="skill-title">
+      { this.props.basic ?
+        <a className="skill-purchase" href="javascript:void(0)" onClick={() => this.props.onSkillPurchased(this.props.skill)}>{this.props.skill.name}</a>:
+        this.props.skill.name
+      }</div>
       <p className="skill-description-tooltip">{this.props.skill.description}</p>
       { this.props.skill.multiplier ? <p>Increases {this.props.skill.team ? '' : 'company'} efficiency {this.props.skill.team ?  `of the ${this.props.skill.team} Team` : ''} by {this.props.skill.multiplier * 100}%.</p> : <div /> }
-      <div>Cost: {this.props.skill.cost} views</div>
-      <a className="skill-purchase" href="javascript:void(0)" onClick={() => this.props.onSkillPurchased(this.props.skill)}>Train skill</a>
+      { this.props.basic ?
+        <div /> :
+        <div>Cost: {this.props.skill.cost} views</div>
+      }
+      { this.props.basic ?
+        <div />:
+        <a className="skill-purchase" href="javascript:void(0)" onClick={() => this.props.onSkillPurchased(this.props.skill)}>Train skill</a>
+      }
     </div>;
   }
 
