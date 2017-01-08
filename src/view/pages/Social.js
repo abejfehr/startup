d6cb36531937ecd4608f9d5642f1ca98f6e2b483
Abe Fehr <abe.fehr@gmail.com>
2017-01-08 01:28:42 -0500

    Make social better

--- src/view/pages/Social.js
+++ src/view/pages/Social.js
@@ -4,44 +4,54 @@ class Social extends Component {
 
   constructor (props) {
     super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+      newsFeed: false,
+    }
+  }
+
+  handleLogin () {
+    if (!username || !password) {
+      alert('You must put in your username and password.');
+    } else {
+      this.setState({
+        newsFeed: true,
+      });
+    }
   }
 
   render () {
-    return  <div>
-            { this.props.skills.find(el => el == 'html2') ?
+    return  <div className="social-page">
               <div className="header">
-                <h1 className="headline">{ this.props.skills.find(el => el == "startup") ? 'Startup' : 'My Website' }</h1>
-                <p className="tagline">{ this.props.skills.find(el => el == "startup") ? 'A serious business.' : 'My personal page' }</p>
-                <hr />
-              </div> :
-              <div />
-            }
-            { this.props.totalViews > 0 ?
-              <div>
-                <p>
-                  This page has been viewed {this.props.views} time{this.props.views !== 1 ? 's' : ''}.
-                </p>
-                { this.props.viewsPerSecond > 0 ?
+                <div>
+                  <h1 className="headline">StartUp</h1>
+                  <p className="tagline">A place for people...and cats.</p>
+                </div>
+              </div>
+              <div className="social-page-content-container">
+                <div className="left-side">
+                  <h2>Share cat pictures and spy on people.</h2>
+                  <h3>Join your friends today.</h3>
+                  <p>This is just a bunch of BS about social networks that you can read. We want you to join today so you can sell us your data and feel obligated to keep in touch with people you hate.</p>
+                  <br />
                   <p>
-                    You are earning {this.props.viewsPerSecond.toFixed(1)} view{this.props.viewsPerSecond !== 1 ? 's' : ''} per second.
-                  </p> :
-                  <div />
-                }
-              </div> :
-              <div />
-            }
-            { this.props.skills.find(el => el == 'html2') && ! this.props.skills.find(el => el == 'startup') ?
-              <img src="assets/pikachu.gif" /> :
-              <div />
-            }
-            { this.props.skills.find(el => el == 'html2') ?
+                    Our social network has {this.props.views} member{this.props.views !== 1 ? 's' : ''}, and is growing by {this.props.viewsPerSecond.toFixed(1)} member{this.props.viewsPerSecond !== 1 ? 's' : ''} each second!
+                  </p>
+                  <span className="icon-globe"></span>
+                </div>
+                <div className="right-side">
+                  <h3>Sign In</h3>
+                  <input onInput={(e) => this.setState({ username: e.target.value })} value={this.state.username} placeholder="Username" />
+                  <br />
+                  <input onInput={(e) => this.setState({ password: e.target.value })} value={this.state.password} placeholder="Password" type="password" />
+                  <br />
+                  <button onClick={this.handleLogin.bind(this)}>Login</button>
+                  <a href="javascript:void(0)">Forgot your password?</a>
+                </div>
+              </div>
               <div className="footer">
-                <hr />
-                <p>
-                  Copyright &copy; {(new Date()).getFullYear()} { this.props.skills.find(el => el == "startup") ? 'Startup' : 'My Website' }. All Rights Reserved. <a href="javascript:void(0)" onClick={() => this.props.onReset()}>
-                    Reset
-                  </a>
-                </p>
                 { this.props.skills.find(el => el == 'monetize') ?
                   <div style={{
                     display: 'inline-block',
@@ -50,10 +60,14 @@ class Social extends Component {
                     border: '1px solid black',
                     backgroundColor: '#aaa'
                   }} /> :
-                  <div />}
-              </div> :
-              <div />
-            }
+                  <div />
+                }
+                <p>
+                  Copyright &copy; {(new Date()).getFullYear()} StartUp. All Rights Reserved. <a href="javascript:void(0)" onClick={() => this.props.onReset()}>
+                    Reset
+                  </a>
+                </p>
+              </div>
             </div>
   }
 
