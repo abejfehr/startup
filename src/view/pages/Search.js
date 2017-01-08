60d4f1599ba9991febdbdd2824a2a973fa9de953
Abe Fehr <abe.fehr@gmail.com>
2017-01-07 21:49:00 -0500

    Make search page beautiful

--- src/view/pages/Search.js
+++ src/view/pages/Search.js
@@ -9,25 +9,23 @@ class Search extends Component {
   render () {
     return  <div className="search-page">
               <div>
-                <h1>startup</h1>
-                <input />
-                <button>search</button>
+                <h1>
+                  <span className="letter-1">S</span>
+                  <span className="letter-2">t</span>
+                  <span className="letter-3">a</span>
+                  <span className="letter-4">r</span>
+                  <span className="letter-5">t</span>
+                  <span className="letter-6">u</span>
+                  <span className="letter-7">p</span>
+                </h1>
+                <input placeholder=" Enter your search query here" />
+                <br />
+                <button>Search</button>
               </div>
-              <div>
-                <p>
-                  This page has been viewed {this.props.views} time{this.props.views !== 1 ? 's' : ''}.
-                </p>
-                <p>
-                  You are earning {this.props.viewsPerSecond.toFixed(1)} view{this.props.viewsPerSecond !== 1 ? 's' : ''} per second.
-                </p>
-              </div>
-            { this.props.skills.find(el => el == 'html2') && ! this.props.skills.find(el => el == 'startup') ?
-              <img src="assets/pikachu.gif" /> :
-              <div />
-            }
-            { this.props.skills.find(el => el == 'html2') ?
               <div className="footer">
-                <hr />
+                <p>
+                  {this.props.views} search{this.props.views !== 1 ? 'es' : ''} made, with {this.props.viewsPerSecond.toFixed(1)} search{this.props.viewsPerSecond !== 1 ? 'es' : ''} per second.
+                </p>
                 <p>
                   Copyright &copy; {(new Date()).getFullYear()} { this.props.skills.find(el => el == "startup") ? 'Startup' : 'My Website' }. All Rights Reserved. <a href="javascript:void(0)" onClick={() => this.props.onReset()}>
                     Reset
@@ -42,9 +40,8 @@ class Search extends Component {
                     backgroundColor: '#aaa'
                   }} /> :
                   <div />}
-              </div> :
+              </div>
               <div />
-            }
             </div>
   }
 
