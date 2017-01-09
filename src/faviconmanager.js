310a9e2aa253c8e15dcf730099c00028952d208e
Abe Fehr <abe.fehr@gmail.com>
2017-01-08 20:18:01 -0500

    Add social favicon

--- src/faviconmanager.js
+++ src/faviconmanager.js
@@ -7,11 +7,15 @@ class FaviconManager {
   }
 
   update (skills) {
-    if (skills.indexOf('search') > -1) {
-      if (!this.link.href || (this.link.href && this.link.href.indexOf('search.ico') < 0)) {
-        this.link.href = 'assets/favicons/search.ico';
+    if (skills.indexOf('social') > -1) {
+      if (!this.link.href || (this.link.href && this.link.href.indexOf('social.ico') < 0)) {
+        this.link.href = 'assets/favicons/social.ico';
       }
-    } else if (skills.indexOf('favicon') > -1) {
+    } else if (skills.indexOf('search') > -1) {
+        if (!this.link.href || (this.link.href && this.link.href.indexOf('search.ico') < 0)) {
+          this.link.href = 'assets/favicons/search.ico';
+        }
+      } else if (skills.indexOf('favicon') > -1) {
       if (!this.link.href || (this.link.href && this.link.href.indexOf('basic.ico') < 0)) {
         this.link.href = 'assets/favicons/basic.ico';
       }
