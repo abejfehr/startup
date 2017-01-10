fd6e589e99593f116e12c9eff55d3f649c40258f
Abe Fehr <abe.fehr@gmail.com>
2017-01-09 21:49:41 -0500

    Make all teams show in worker table

---
+++ src/view/components/TeamTable.js
@@ -0,0 +1,47 @@
+import { h, Component } from 'preact';
+
+class TeamTable extends Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    var workers = this.props.workers.map((worker) => {
+      return <tr key={worker.name}>
+        <td>{worker.name}</td>
+        <td><a className="fire-worker" href="javascript:void(0)" onClick={() => this.props.onFire(this.props.team, worker.id)}>
+        { this.props.icon ?
+          <span className="icon-exit"></span> :
+          'fire'
+        }
+        </a></td>
+      </tr>
+    });
+
+    return  <div className="old-table-wrapper">
+              <table className="old-table">
+                <thead>
+                  <tr>
+                    <th colSpan="2">
+                      Graphic Designers<br />
+                      <div className="hire-worker">
+                        <a
+                          href="javascript:void(0)"
+                          onClick={() => this.props.onHire(this.props.team)}>
+                          [Hire Employee]
+                        </a> ({this.props.cost} views)
+                      </div>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {workers}
+                </tbody>
+              </table>
+            </div>;
+  }
+
+}
+
+export default TeamTable;
--- src/view/components/WorkersTable.js
+++ src/view/components/WorkersTable.js
@@ -1,5 +1,7 @@
 import { h, Component } from 'preact';
 
+import TeamTable from './TeamTable';
+
 import TeamType from '../../teamtype';
 
 class WorkersTable extends Component {
@@ -9,42 +11,19 @@ class WorkersTable extends Component {
   }
 
   render () {
-    var workers = this.props.teams[TeamType.GRAPHIC_DESIGN].workers.map((worker) => {
-      return <tr key={worker.name}>
-        <td>{worker.name}</td>
-        <td><a className="fire-worker" href="javascript:void(0)" onClick={() => this.props.onFire(TeamType.GRAPHIC_DESIGN, worker.id)}>
-        { this.props.icon ?
-          <span className="icon-exit"></span> :
-          'fire'
-        }
-        </a></td>
-      </tr>
-    });
+    if (this.props.teams) {
+      var teams = Object.keys(this.props.teams).map(function (team) {
+        return  <TeamTable
+                  workers={this.props.teams[team].workers}
+                  team={team}
+                  cost={this.props.teams[team].getCost()} />;
+      }.bind(this));
+    }
 
     return  <div className="worker-table">
               <div>
                 <h2>Employees</h2>
-                <div className="old-table-wrapper">
-                  <table className="old-table">
-                    <thead>
-                      <tr>
-                        <th colSpan="2">
-                          Graphic Designers<br />
-                          <div className="hire-worker">
-                            <a
-                              href="javascript:void(0)"
-                              onClick={() => this.props.onHire(TeamType.GRAPHIC_DESIGN)}>
-                              [Hire Employee]
-                            </a> ({this.props.teams[TeamType.GRAPHIC_DESIGN].getCost()} views)
-                          </div>
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {workers}
-                    </tbody>
-                  </table>
-                </div>
+                { teams }
               </div>
             </div>;
   }
