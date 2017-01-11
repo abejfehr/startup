dcb1e0222c1c20b520cc284e6fe01ea7817824f7
Abe Fehr <abe.fehr@gmail.com>
2017-01-10 21:18:09 -0500

    Complete about page

--- src/view/components/About.js
+++ src/view/components/About.js
@@ -8,9 +8,15 @@ class About extends Component {
 
   render () {
     return  <div className="about-page">
-              <a href="javascript:void(0)" onClick={this.props.goBack}>Go back</a>
-              <h2>About this thing</h2>
+              <a href="javascript:void(0)" onClick={this.props.onGoBack}>&lt; Go back</a>
+              <h1>About this thing</h1>
               <p>Startup is a game that was developed by <a href="https://www.abefehr.com/">Abe Fehr</a> and <a href="javascript:void(0)" onClick={() => alert("He's shy and doesn't give out his info. Sorry!")}>Scott Andrechek</a></p>
+              <div className="footer">
+                <hr />
+                <p>
+                  Copyright &copy; {(new Date()).getFullYear()} {this.props.title}. All Rights Reserved.
+                </p>
+              </div>
             </div>;
   }
 }
--- src/view/pages/Basic.js
+++ src/view/pages/Basic.js
@@ -1,5 +1,7 @@
 import { h, Component } from 'preact';
 
+import About from '../components/About';
+
 class Basic extends Component {
 
   constructor (props) {
@@ -10,19 +12,52 @@ class Basic extends Component {
     }
   }
 
-  goBack () {
-    this.setState({
-      aboutShown: false,
-    });
+  goBack (e) {
+    var oldCursor = e.target.style.cursor;
+
+    // Don't do this twice
+    if (oldCursor == 'wait') { return; }
+
+    document.body.style.cursor = 'wait';
+    e.target.style.cursor = 'wait';
+
+
+    setTimeout(
+      () => {
+        this.setState({
+          aboutShown: false,
+        });
+        document.body.style.cursor = 'default';
+        e.target.style.cursor = oldCursor;
+      }, 500
+    )
   }
 
-  showAbout () {
-    this.setState({
-      aboutShown: true,
-    });
+  showAbout (e) {
+    var oldCursor = e.target.style.cursor;
+
+    // Don't do this twice
+    if (oldCursor == 'wait') { return; }
+
+    // Start "loading"
+    document.body.style.cursor = 'wait';
+    e.target.style.cursor = 'wait';
+
+    setTimeout(
+      () => {
+        this.setState({
+          aboutShown: true,
+        });
+        document.body.style.cursor = 'default';
+        e.target.style.cursor = oldCursor;
+      }, 500
+    )
   }
 
   render () {
+    if (this.state.aboutShown) {
+      return <About title={ this.props.skills.find(el => el == "startup") ? 'Startup' : 'My Website' } onGoBack={this.goBack.bind(this)} />;
+    }
     return  <div>
             { this.props.skills.find(el => el == 'html2') ?
               <div className="header">
@@ -32,33 +67,28 @@ class Basic extends Component {
               </div> :
               <div />
             }
-            { !this.state.aboutShown ?
+            { this.props.totalViews > 0 ?
               <div>
-                { this.props.totalViews > 0 ?
-                  <div>
-                    <p>
-                      This page has been viewed {this.props.views} time{this.props.views !== 1 ? 's' : ''}.
-                    </p>
-                    { this.props.viewsPerSecond > 0 ?
-                      <p>
-                        You are earning {this.props.viewsPerSecond.toFixed(2)} view{this.props.viewsPerSecond !== 1 ? 's' : ''} per second.
-                      </p> :
-                      <div />
-                    }
-                  </div> :
-                  <div />
-                }
-                { this.props.skills.find(el => el == 'html2') && ! this.props.skills.find(el => el == 'about') ?
-                  <img src="assets/pikachu.gif" /> :
-                  <div />
-                }
-                {
-                  this.props.skills.find(el => el == 'about') ?
-                  <a href="javascript:void(0)" onClick={this.showAbout}>About</a> :
+                <p>
+                  This page has been viewed {this.props.views} time{this.props.views !== 1 ? 's' : ''}.
+                </p>
+                { this.props.viewsPerSecond > 0 ?
+                  <p>
+                    You are earning {this.props.viewsPerSecond.toFixed(2)} view{this.props.viewsPerSecond !== 1 ? 's' : ''} per second.
+                  </p> :
                   <div />
                 }
               </div> :
-              <About onGoBack={this.goBack} />
+              <div />
+            }
+            { this.props.skills.find(el => el == 'html2') && ! this.props.skills.find(el => el == 'about') ?
+              <img src="assets/pikachu.gif" /> :
+              <div />
+            }
+            {
+              this.props.skills.find(el => el == 'about') ?
+              <a href="javascript:void(0)" onClick={this.showAbout.bind(this)}>About</a> :
+              <div />
             }
             { this.props.skills.find(el => el == 'html2') ?
               <div className="footer">
